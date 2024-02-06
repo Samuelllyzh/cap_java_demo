2f22f2106fb9773f46809f2fb15b47006f2d2181
Samuelllyzh <yzh.samuel97@gmail.com>
2024-02-06 02:36:50 +0000

    securityConfig追加

---
+++ srv/src/main/java/customer/cap_java_demo/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package customer.cap_java_demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        http.formLogin(login -> login
+                .loginProcessingUrl("/login")
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error")
+                .permitAll()).logout(logout -> logout
+                        .logoutSuccessUrl("/"))
+                .authorizeHttpRequests(authz -> authz
+
+                        // URLごとの認可設定記述開始
+
+                        .anyRequest().permitAll()
+                // 他のURLはログイン後のみアクセス可能
+
+                );
+        return http.build();
+    }
+}
