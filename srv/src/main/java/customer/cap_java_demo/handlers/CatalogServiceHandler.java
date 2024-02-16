207db5da1cfd9ed550a37f1bc89dfd361c949615
Samuelllyzh <yzh.samuel97@gmail.com>
2024-02-16 01:17:45 +0000

    更新create方法，获取req中的参数  并且插入到其他表中

--- srv/src/main/java/customer/cap_java_demo/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/customer/cap_java_demo/handlers/CatalogServiceHandler.java
@@ -1,9 +1,12 @@
 package customer.cap_java_demo.handlers;
 
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 import java.util.stream.Stream;
 
 import org.springframework.stereotype.Component;
@@ -33,9 +36,9 @@ public class CatalogServiceHandler implements EventHandler {
 	Result result;
 	CqnService service;
 	private final PersistenceService db;
-	private Map<Object, Map<String, Object>> books = new HashMap<>();
+	private Map<Object, Map<String, Object>> bookListMap = new HashMap<>();
 
-	CatalogServiceHandler(PersistenceService db){
+	CatalogServiceHandler(PersistenceService db) {
 		this.db = db;
 	}
 
@@ -46,41 +49,52 @@ public class CatalogServiceHandler implements EventHandler {
 	}
 
 	// 写在插入和更新写在一起的方法
-	// @Before(event = { CqnService.EVENT_CREATE, CqnService.EVENT_UPDATE }, entity = Books_.CDS_NAME)
+	// @Before(event = { CqnService.EVENT_CREATE, CqnService.EVENT_UPDATE }, entity
+	// = Books_.CDS_NAME)
 	// public void changeBooks(EventContext context) {
-	// 	if(context.getEvent().equals(CqnService.EVENT_CREATE)) {
-	// 		CdsCreateEventContext ctx = context.as(CdsCreateEventContext.class);
-	// 		// ...
-	// 	} else {
-	// 		CdsUpdateEventContext ctx = context.as(CdsUpdateEventContext.class);
-	// 		// ...
-	// 	}
+	// if(context.getEvent().equals(CqnService.EVENT_CREATE)) {
+	// CdsCreateEventContext ctx = context.as(CdsCreateEventContext.class);
+	// // ...
+	// } else {
+	// CdsUpdateEventContext ctx = context.as(CdsUpdateEventContext.class);
+	// // ...
+	// }
 	// }
 
 	@On(event = CqnService.EVENT_CREATE, entity = Books_.CDS_NAME)
-	public void addBooks(CdsCreateEventContext context, List<Books> books) {
+	public void addBooks(CdsCreateEventContext createContext, List<Books> books) {
 		System.out.println("----------------------CREATE----------------------");
-		// try {
-		// 	// AddBooks books = reqContext.get(0);
-		// 	// req.getParameterInfo().getQueryParams();
-		// 	System.out.println(reqContext.getCqn().entries());
-		// 	// reqContext.getCqn().entries().forEach(e -> books.put(e.get("ID"), e));
-		// 	Map<String, String> paramValuesss = reqContext.getParameterInfo().getQueryParams();
-		// 	Map<String, Object> bookInfo = new HashMap<>();
-		// 	bookInfo.put("title", "10");
-		// 	bookInfo.put("stock", 111);
-	
-		// 	CqnInsert insert = Insert.into("addBooks").entry(bookInfo);
-		// 	List<Map<String, Object>> resultList = new ArrayList<>();
-		// 	resultList = insert.entries();
-		// 	result = db.run(insert);
-		// 如果有namespace的话，表名字前面要加namespace
-		// 	CqnInsert insert = Insert.into("my.bookshop.LogsTable").entry(paramValues);
-		// 	db.run(insert);
-		// 	System.out.println(result);
-		// } catch (Exception e) {
-		// 	System.out.println(e);
-		// }
+		try {
+			// 通过createContext获取请求中的参数
+			createContext.getCqn().entries().forEach(e -> bookListMap.put(e.get("ID"), e));
+			System.out.println(bookListMap);
+
+			// 获取当前时间(GMT+0)
+			String nowTimeStr = getNowTimeStr();
+			// 操作ログ(get req中各种参数)
+			String logStr = "操作ログ「:" + "\r\n" +
+					"   ★実行時間		:" + nowTimeStr + "\r\n" +
+					"   ★イベント名		:" + createContext.getEvent() + "\r\n" +
+					"   ★エンティティ名	:" + createContext.getTarget().getQualifiedName() + "\r\n" +
+					"   ★パラメータ		:" + createContext.getCqn().entries() + "\r\n" +
+					"   ★ユーザID		:" + createContext.getUserInfo().getId() + "\r\n" +
+					"	★ユーザ名称		:" + createContext.getUserInfo().getName() + "\r\n" +
+					"」";
+			System.out.println(logStr);
+
+			Map<String, Object> logMessageInfo = new HashMap<>();
+			logMessageInfo.put("message", logStr);
+			/**
+			 * 准备insert语句
+			 * 如果有namespace的话，表名字前面要加namespace
+			 * Insert.into("my.bookshop.LogMessage").entry(logMessageInfo);
+			 **/
+			CqnInsert insertToLogMessage = Insert.into("LogMessage").entry(logMessageInfo);
+			result = db.run(insertToLogMessage);
+			System.out.println(result);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 	@On(event = CqnService.EVENT_UPDATE, entity = Books_.CDS_NAME)
@@ -93,71 +107,12 @@ public class CatalogServiceHandler implements EventHandler {
 		System.out.println("----------------------DELETE----------------------");
 	}
 
-	// @On(event = {CqnService.EVENT_READ}, entity = AddBooks_.CDS_NAME)
-	// public void readAddBooks(CdsCreateEventContext reqContext) {
-	// 	System.out.println("aaa");
-	// }
-
-	// @On(event = CqnService.EVENT_CREATE, entity = "CatalogService.addBooks")
-	// public void addBooks1(CdsCreateEventContext reqContext) {
-	// 	try {
-	// 		// AddBooks books = reqContext.get(0);
-	// 		// req.getParameterInfo().getQueryParams();
-	// 		System.out.println(reqContext.getCqn().entries());
-	// 		// reqContext.getCqn().entries().forEach(e -> books.put(e.get("ID"), e));
-	// 		Map<String, String> paramValuesss = reqContext.getParameterInfo().getQueryParams();
-	// 		Map<String, Object> bookInfo = new HashMap<>();
-	// 		bookInfo.put("title", "10");
-	// 		bookInfo.put("stock", 111);
-	
-	// 		CqnInsert insert = Insert.into("addBooks").entry(bookInfo);
-	// 		List<Map<String, Object>> resultList = new ArrayList<>();
-	// 		resultList = insert.entries();
-	// 		result = db.run(insert);
-	// 		System.out.println(result);
-	// 	} catch (Exception e) {
-	// 		System.out.println(e);
-	// 	}
-	// }
-
-	// @On(event = CqnService.EVENT_CREATE, entity = "CatalogService.addBooks")
-	// public void addBooks(CdsCreateEventContext reqContext) {
-	// 	try {
-	// 		System.out.println(reqContext.getCqn().entries());
-
-	// 		Map<String, Object> messageInfo = new HashMap<>();
-	// 		messageInfo.put("message", "1233445566");
-	
-	// 		CqnInsert insert = Insert.into("message").entry(messageInfo);
-	// 		List<Map<String, Object>> resultList = new ArrayList<>();
-	// 		resultList = insert.entries();
-	// 		result = db.run(insert);
-	// 	} catch (Exception e) {
-	// 		System.out.println(e);
-	// 	}
-	// }
-
-
-	// @On(event = CqnService.EVENT_CREATE, entity = "CatalogService.updateBooks")
-	// public void updateBooks(EventContext req) {
-	// 	Map<String, Object> paramValues = new HashMap<>();
-	// 	paramValues.put("LogText", "103");
-	// 	CqnInsert insert = Insert.into("my.bookshop.LogsTable").entry(paramValues);
-	// 	db.run(insert);
-	// }
-
-	// @On(event = "*", entity = "CatalogService.deleteBooks")
-	// public void deleteBooks(EventContext req) {
-	// 	// DeleteBooks deleteBooks = req.get(0);
-	// 	// System.out.println(deleteBooks.getTitle());
-	// 	// System.out.println(deleteBooks.getStock());
-	// 	// Map<String, Object> paramValues = new HashMap<>();
-	// 	// Map<String, String> paramValuesss = req.getParameterInfo().getQueryParams();
-	// 	// String a  = paramValuesss.get("ID");
-	// 	// paramValues.put("LogText", "103");
-
-	// 	// CqnInsert insert = Insert.into("my.bookshop.LogsTable").entry(paramValues);
-	// 	// db.run(insert);
-	// }
+	// 获取当前时间(GMT+0)
+	public String getNowTimeStr() {
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS")
+				.withZone(TimeZone.getTimeZone("GMT+0").toZoneId());
+		String nowTimeStr = df.format(Instant.now());
+		return nowTimeStr;
+	}
 
 }
